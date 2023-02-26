a520e3774c03c5e068448c42e18f33c242e09045
vboxuser <vboxuser@WINDA10>
2023-02-26 15:03:28 +0500

    Добавьте файлы проекта.

---
+++ 1.1/1.1.cpp
@@ -0,0 +1,27 @@
+﻿// 1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int N = 10;
+    int A[N] = { 4, 8, 1, 34, 89, 10, 13, 19, 18, 20 };
+    int B[N];
+    int* a = A;
+    int* b  = B;
+   
+
+    cout << "*****Второй массив******\n";
+    
+    for (int i = 0; i < N;a++, b++, i++)
+    {
+        b = a;
+        cout << *b << " " ;
+       
+    }
+    cout << "\n\n";
+
+  
+}
+
